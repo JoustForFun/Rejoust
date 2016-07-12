2326e35349526d3f3709f38039f68fa30aa67693
Francesca <fcoddo17@gmail.com>
2016-07-12 11:19:29 -0400

    enemy2

--- Assets/Scripts/enemyAI_1.cs
+++ Assets/Scripts/enemyAI_1.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class enemyAI_1 : MonoBehaviour {
 	
-	 Rigidbody2D rb1; 
+	Rigidbody2D rb1; 
 	public float enemy_y; 
 	public float player_y;
 	public float player_x;
@@ -18,7 +18,6 @@ public class enemyAI_1 : MonoBehaviour {
 		rb1 = GetComponent<Rigidbody2D> ();
 		enemy_y = transform.position.y;
 		player_y = GameObject.Find ("player").transform.position.y;
-		Debug.Log (player_y);
 		begin_diff = player_y - enemy_y;
 	    
 	}
@@ -32,7 +31,6 @@ public class enemyAI_1 : MonoBehaviour {
 		transform.position += new Vector3((player_x-transform.position.x),0,0) * Time.deltaTime * sometimes_x;
 
 		if (player_y - enemy_y>=begin_diff-0.1f) { //fly upwards if player height is greater than enemy height
-			Debug.Log("time to fly");
 			Flying (); 
 		}
 		else if (player_y - enemy_y<begin_diff-0.1f) { //attack if player height is less than enemy height
