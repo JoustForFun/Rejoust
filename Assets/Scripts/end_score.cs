b3c936a2a784e3513a5b8ff80a6c4acc9e98de07
Francesca <fcoddo17@gmail.com>
2016-07-14 19:42:05 -0400

    End scene

---
+++ Assets/Scripts/end_score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Lib; 
+
+public class end_score : MonoBehaviour {
+	Text mytext; 
+
+	// Use this for initialization
+	void Awake() {
+		mytext = gameObject.GetComponent<Text> ();
+
+	}
+
+	void Start () {
+		mytext.text = "YOUR SCORE: " + 	Reference.FINAL_SCORE;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
