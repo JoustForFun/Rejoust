1afcc082548bf788a11e5ce3e62bf636c2cbad0a
Francesca <fcoddo17@gmail.com>
2016-07-14 20:10:25 -0400

    start menu audio

---
+++ Assets/Scripts/start_audio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using JAudio;
+using Lib;
+
+public class start_audio : MonoBehaviour {
+
+
+	void Awake() {
+		AudioManager.INSTANCE.PlayAudio (Audio.MENU_SONG, true); 
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
